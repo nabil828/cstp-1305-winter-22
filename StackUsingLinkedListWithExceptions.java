601177e0c86ae3373d2449c32c0cc470dc4f8250
Nabil M. Al-Rousan <rousani@gmail.com>
2022-01-20 15:23:09 -0800

    Generics

--- StackUsingLinkedListWithExceptions.java
+++ StackUsingLinkedListWithExceptions.java
@@ -1,10 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
 
 // This stack will hold up to 5 items only
 // todo ..
-public class StackUsingLinkedListWithExceptions implements StackWithExceptionsInterface{
+public class StackUsingLinkedListWithExceptions<T> implements StackWithExceptionsInterface<T>{
 
     // to use a linked list as data structure/cdt
-    Node top;
+    NodeGeneric<T> top;
 
     StackUsingLinkedListWithExceptions() { // Constructors are used in classes to initialize data members
         top = null;
@@ -13,10 +16,10 @@ public class StackUsingLinkedListWithExceptions implements StackWithExceptionsIn
     int size = 0;
 
     // todo specifications should be provided
-    public void push(int a) throws PushingIntoFullStackCheckedException {
+    public void push(T a) throws PushingIntoFullStackCheckedException {
         if (size == 5)
             throw new PushingIntoFullStackCheckedException("PushingIntoFullStackCheckedException");
-        Node temp = new Node();
+        NodeGeneric<T> temp = new NodeGeneric<T>();
         temp.data = a;
         temp.next = null;
 
@@ -33,10 +36,10 @@ public class StackUsingLinkedListWithExceptions implements StackWithExceptionsIn
 
     // todo specifications should be provided
     // return -1 if the stack is empty .. more on this later when we talk about exceptions
-    public int pop() throws AccessingAnEmptyStackCheckedException {
+    public T pop() throws AccessingAnEmptyStackCheckedException {
         if (size == 0)
             throw new AccessingAnEmptyStackCheckedException("AccessingAnEmptyStackCheckedException");
-        int temp = top.data;
+        T temp = (T) top.data;
         top = top.next;
 
         size--;
@@ -45,12 +48,8 @@ public class StackUsingLinkedListWithExceptions implements StackWithExceptionsIn
 
     // todo specifications should be provided
     // return -1 if the stack is empty .. more on this later when we talk about exceptions
-    public int peek() {
-        if (top == null) {
-            //stack is empty
-            return -1;
-        } else
-            return top.data;
+    public T peek() {
+        return (T) top.data;
     }
 
 
@@ -58,7 +57,7 @@ public class StackUsingLinkedListWithExceptions implements StackWithExceptionsIn
         System.out.println("Hello, this is my first Java code!");
 
         try {
-            StackUsingLinkedListWithExceptions object_stack = new StackUsingLinkedListWithExceptions();
+            StackUsingLinkedListWithExceptions<Integer> object_stack = new StackUsingLinkedListWithExceptions<Integer>();
             object_stack.push(6);
             object_stack.push(1);
             object_stack.push(7);
@@ -67,13 +66,14 @@ public class StackUsingLinkedListWithExceptions implements StackWithExceptionsIn
             System.out.println(object_stack.peek()); // we are expecting the 7 to be printed
 
             String postfix_expr = "512+4*+3-";
-            StackWithExceptionsInterface x = new StackUsingLinkedListWithExceptions();
+            StackWithExceptionsInterface<Integer> x = new StackUsingLinkedListWithExceptions<Integer>();
+
             for (int i = 0; i < postfix_expr.length(); i++) {
                 char next = postfix_expr.charAt(i);
 
                 if (next == '+' || next == '-' || next == '/' || next == '*') {
-                    int op1 = (x.pop());
-                    int op2 = (x.pop());
+                    Integer op1 = (Integer) (x.pop());
+                    Integer op2 = (Integer) (x.pop());
                     switch (next) {
                         case '+':
                             x.push(op2 + op1);
--- StackWithExceptionsInterface.java
+++ StackWithExceptionsInterface.java
@@ -2,11 +2,11 @@
 // this stack has a limit of 5 items at max
 // it should raise an exception whenever this limit=5 is reached
 // it would also raise an exception whenever we peek or pop from an empty stack
-public interface StackWithExceptionsInterface {
+public interface StackWithExceptionsInterface<T> {
     // push – insert an item at the top of the stack
-    // @throws PushingIntoFullStackCheckedException exceptoion whenever it is full
-    public void push (int a) throws PushingIntoFullStackCheckedException;
+    // @throws PushingIntoFullStackCheckedException exceptoion whenever it is fullge
+    public void push (T a) throws PushingIntoFullStackCheckedException;
 
-    public int pop() throws AccessingAnEmptyStackCheckedException;
-    public int peek();
+    public T pop() throws AccessingAnEmptyStackCheckedException;
+    public T peek();
 }
