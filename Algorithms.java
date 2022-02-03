107a2249d75454c56b599517f52385d5e01b253f
Nabil M. Al-Rousan <rousani@gmail.com>
2022-02-03 14:58:19 -0800

    Selection Sort

--- Algorithms.java
+++ Algorithms.java
@@ -1,4 +1,11 @@
 public class Algorithms {
+
+    static void swap(int[] arr, int a , int b){
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
     static int linear_search(int arr[], int target){
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] == target)
@@ -31,6 +38,32 @@ public class Algorithms {
         return  -1;
 
     }
+
+    static void bubble_sort(int []arr){
+        for(int i = arr.length - 1 ; i > 0 ; i-- ){
+            for(int j = 0 ; j < i; j++){
+                if(arr[j] > arr[j+1])
+                    swap(arr, j, j +1);
+            }
+        }
+    }
+
+    static void selection_sort(int [] arr){
+        for(int i = 0; i < arr.length ; i++){
+
+            // find the smallest element
+            int min = 999999;
+            int smallest_index = -1;
+            for(int j = i ; j < arr.length; j++){
+                if(arr[j] < min){
+                    min = arr[j];
+                    smallest_index = j;
+                }
+            }
+
+            swap(arr, i, smallest_index);
+        }
+    }
     public static void main(String[] args) {
         int []arr = new int [10];
 //        arr = new int[]{10, 20, 30, 15, 25, 3};
@@ -40,10 +73,21 @@ public class Algorithms {
 //        System.out.println(linear_search(arr, 44)); // index 5
 
 
-        arr = new int[]{10, 12, 30, 41, 59, 60};
+//        arr = new int[]{10, 12, 30, 41, 59, 60};
+//
+//         int target = 12;
+//        System.out.println(binary_search(arr, target));// 1 - index
 
-         int target = 12;
-        System.out.println(binary_search(arr, target));// 1 - index
+//        arr = new int[]{ 12,  59, 6, 30, 15, 41};
+//        bubble_sort(arr); //
+
+        arr = new int[]{ 12,  59, 6, 30, 15, 41};
+        selection_sort(arr); //
+
+        for(int x : arr){
+            System.out.println(x);
+        }
+        // 6 , 12, 15, 30, 41, 59
 
 
     }
