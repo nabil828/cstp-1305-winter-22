63238a8498c15e1fe1263a8969564d72028bb347
Nabil M. Al-Rousan <rousani@gmail.com>
2022-02-03 15:38:38 -0800

    Insertion Sort

--- Algorithms.java
+++ Algorithms.java
@@ -66,33 +66,33 @@ public class Algorithms {
     }
 
     static void insertion_sort(int [] arr){
-//        for(int i = 1 ; i < arr.length ; i++){
-//
-//            int j = i;
-//            int tmp = arr[j];
-//            while (arr[j - 1] > arr [j] && j -1 > 0){
-//                // shift
-//                arr[j] = arr[j -1];
-//                j--;
-//            }
-//            // place
-//            arr[j] = tmp;
-//
-//        }
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
+        for(int i = 1 ; i < arr.length ; i++){
 
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+            int j = i;
+            int tmp = arr[j];
+            while (j  > 0 && arr[j - 1] > tmp ){
+                // shift
+                arr[j] = arr[j -1];
+                j--;
             }
-            arr[j + 1] = key;
+            // place
+            arr[j] = tmp;
+
         }
+//        int n = arr.length;
+//        for (int i = 1; i < n; ++i) {
+//            int key = arr[i];
+//            int j = i - 1;
+//
+//            /* Move elements of arr[0..i-1], that are
+//               greater than key, to one position ahead
+//               of their current position */
+//            while (j >= 0 && arr[j] > key) {
+//                arr[j + 1] = arr[j];
+//                j = j - 1;
+//            }
+//            arr[j + 1] = key;
+//        }
     }
     public static void main(String[] args) {
         int []arr = new int [10];
