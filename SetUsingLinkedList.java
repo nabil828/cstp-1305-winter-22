f0006ebf494c83db825820ecf6a885ed4948a0de
Nabil M. Al-Rousan <rousani@gmail.com>
2022-02-17 10:27:00 -0800

    Non-core operations LinkedList

--- SetUsingLinkedList.java
+++ SetUsingLinkedList.java
@@ -51,6 +51,53 @@ public class SetUsingLinkedList {
         return size;
     }
 
+    int[] toArray(){
+        int []result_array = new int[size];
+        Node i = head;
+        int counter =0;
+        while(i != null) {
+            result_array[counter++] = i.data;
+            i = i.next;
+        }
+
+        return  result_array;
+    }
+
+    public static SetUsingLinkedList union(SetUsingLinkedList aSet, SetUsingLinkedList anotherSet){
+        SetUsingLinkedList result = new SetUsingLinkedList();
+        int [] anotherSet_array = anotherSet.toArray();
+        for(int i = 0 ; i < anotherSet_array.length; i++){
+            result.add(anotherSet_array[i]);
+        }
+
+        int [] aSet_array = aSet.toArray();
+        for (int i = 0; i < aSet_array.length; i++){
+            result.add(aSet_array[i]);
+        }
+        return result;
+    }
+
+    public static SetUsingLinkedList difference(SetUsingLinkedList aSet, SetUsingLinkedList anotherSet){
+        SetUsingLinkedList result = new SetUsingLinkedList();
+        int [] aSet_array = aSet.toArray();
+        for (int i = 0; i < aSet_array.length; i++){
+            if(!anotherSet.contains(aSet_array[i]))
+                result.add(aSet_array[i]);
+
+        }
+        return result;
+    }
+
+    public static SetUsingLinkedList intersection(SetUsingLinkedList aSet, SetUsingLinkedList anotherSet){
+        SetUsingLinkedList result = new SetUsingLinkedList();
+        int [] aSet_array = aSet.toArray();
+        for(int i = 0 ;  i < aSet_array.length ; i++){
+            if(anotherSet.contains(aSet_array[i]))
+                result.add(aSet_array[i]);
+        }
+
+        return result;
+    }
 
 
     public static void main(String[] args) {
@@ -69,12 +116,24 @@ public class SetUsingLinkedList {
         System.out.println(aSet.getSize()); // 2
         aSet.add(5);
         aSet.add(9);
-        aSet.add(80); // size 5
-        System.out.println(aSet.getSize()); // 5
-        aSet.remove(80);
-        System.out.println(aSet.getSize()); // 4
-        aSet.remove(1);
-        System.out.println(aSet.getSize()); // 4
+        aSet.add(80);
+
+        // 3, 2, 50, 9, 80
+
+
+        // Union/addAll, Intersection/retainAll, and Difference/removeAll
+        SetUsingLinkedList anotherSet = new SetUsingLinkedList();
+        anotherSet.add(1);
+        anotherSet.add(44);
+
+        SetUsingLinkedList newSet = union(aSet, anotherSet); // aSet should have  // 3, 2, 50, 9, 80, 1, 44
+        newSet = difference(aSet, anotherSet); // aSet should have  // 3, 2, 50, 9, 80
+        aSet.add(44);
+        newSet = difference(anotherSet, aSet); // aSet should have  // 44
+
+
+        anotherSet.add(80);
+        newSet  = intersection(aSet, anotherSet); // aSet should have 80 and 44
 
     }
 }
