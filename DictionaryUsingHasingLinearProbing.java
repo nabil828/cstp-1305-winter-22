9bbcdb17213b3c412b08e6fbeae9ba03f2f123f3
Nabil M. Al-Rousan <rousani@gmail.com>
2022-03-03 15:59:34 -0800

    Task 1 completed - Task 2 Test Case
    use this as sekelton code for Task 2.
    
    For task 2
    insert(35, "any value");
    insert(60, "any value");
    insert(12, "any value");
    remove(60);
    insert(15, "any value"); //should be inserted at index 15

--- DictionaryUsingHasingLinearProbing.java
+++ DictionaryUsingHasingLinearProbing.java
@@ -1,12 +1,46 @@
+
+enum STATUS {
+    OCCUPIED,
+    AVAILABLE,
+    EMPTY //null
+}
+class PairWithStatus{
+    Pair aPair;
+    STATUS status;
+    public  PairWithStatus(){
+        aPair = new Pair();
+        status = STATUS.EMPTY;
+    }
+
+    public PairWithStatus(Pair aPair, STATUS status) {
+        this.aPair = aPair;
+        this.status = status;
+    }
+
+}
 public class DictionaryUsingHasingLinearProbing {
     final int HASHTABLE_SIZE = 23;
-    Pair arr []  = new Pair [HASHTABLE_SIZE];
+    PairWithStatus arr [] ;
     int size = 0;
+
     public DictionaryUsingHasingLinearProbing(){
-        arr[6].key = 29;
-        arr[9].key = 32;
-        arr[12].key = 58 ;
-        arr[21].key = 21;
+        arr = new PairWithStatus[HASHTABLE_SIZE];
+        arr[6] = new PairWithStatus();
+        arr[6].aPair.key = 29;
+        arr[6].status= STATUS.OCCUPIED;
+
+        arr [9]  = new PairWithStatus();
+        arr[9].aPair.key = 32;
+        arr[9].status= STATUS.OCCUPIED;
+
+        arr [12]  =new PairWithStatus();
+        arr[12].aPair.key = 58 ;
+        arr[12].status= STATUS.OCCUPIED;
+
+        arr [21]  = new PairWithStatus();
+        arr[21].aPair.key = 21;
+        arr[21].status= STATUS.OCCUPIED;
+
 
     }
     int hashCode_(int a){
@@ -18,75 +52,83 @@ public class DictionaryUsingHasingLinearProbing {
         return hashIndex;
 
     }
-
     //
-    void probe(int index, int key){
-//        // Searches the probe sequence that begins at index. Returns the index of
-//                either the element
-//        // containing key or an available element in the hash table.
-//                while (key is not found and hashTable[index] is not null) {
-//                    if (hashTable[index] references an entry in the dictionary) {
-//                        if (the entry in hashTable[index] contains key)
-//                        Exit loop
-//            else
-//                        index = next probe index
-//                    } else // hashTable[index] is available
-//                    {
-//                        if (this is the first available element encountered)
-//                        availableStateIndex = index
-//                        index = next probe index
-//                    }
-//                }
-//                if (key is found or an available element was not encountered)
-//                return index
+    int probe(int index, int key) { //index is integer value the key
+        // Searches the probe sequence that begins at index.
+        // Returns the index of either the element
+        // containing key or an available element in the hash table.
+        boolean found = false;
+
+        while (!found && arr[index].status != STATUS.EMPTY) { //key is not found and hashTable[index] is not null
+//            if (arr[index].status != STATUS.AVAILABLE) { //hashTable[index] references an entry in the dictionary
+                if (arr[index].aPair.key ==  key) //the entry in hashTable[index] contains key
+                    found = true;
+                else {
+                    index = (index + 1) % HASHTABLE_SIZE;//next probe index
+                    found = true;
+                }
+
+//            } else // hashTable[index] is available
+//            {
+//                if (this is the first available element encountered)
+//                availableStateIndex = index
+//                index = next probe index
+//            }
+        }
+
+//        if (key is found or an available element was not encountered)
+        return index;
 //        else
-//                return availableStateIndex // Index of first entry removed
+//            return availableStateIndex // Index of first entry removed
     }
     void insert(int key, String value){
-        if(!this.contains(key)){
+//        if(!this.contains(key)){
             //insertion
-            Pair newPair = new Pair(key, value);
-            arr[size] = newPair;
+            Pair newPiar = new Pair(key, value);
+            PairWithStatus newPairWithStatus = new PairWithStatus(newPiar, STATUS.OCCUPIED);
+            arr[probe(hashCode_(key), key)] = newPairWithStatus;
+
+//            arr[size] = newPair;
             size++;
-        }
+//        }
         // else, nothing
     }
 
-    void remove(int key){
-        if(this.contains(key)){
-            for(int i = 0; i < size ; i++) {
-                if (arr[i].key == key)
-                    arr[i] = arr[size - 1];
-            }
-            size--;
-        }
-        // else, nothing
-    }
+//    void remove(int key){
+//        if(this.contains(key)){
+//            for(int i = 0; i < size ; i++) {
+//                if (arr[i].key == key)
+//                    arr[i] = arr[size - 1];
+//            }
+//            size--;
+//        }
+//        // else, nothing
+//    }
 
-    void modify(int key, String value){
-        for(int i = 0; i < size ; i++) {
-            if (arr[i].key == key)
-                arr[i].value = value;
-            //modify
-        }
-    }
+//    void modify(int key, String value){
+//        for(int i = 0; i < size ; i++) {
+//            if (arr[i].key == key)
+//                arr[i].value = value;
+//            //modify
+//        }
+//    }
 
-    String lookup(int key){
-        for(int i = 0; i < size ; i++) {
-            if (arr[i].key == key){
-                return arr[i].value;
-            }
-            //modify
-        }
-        return "";
-    }
-    boolean contains(int key){
-        for(int i = 0; i < size ; i++){
-            if(arr[i].key == key)
-                return true;
-        }
-        return false;
-    }
+//    String lookup(int key){
+//        for(int i = 0; i < size ; i++) {
+//            if (arr[i].key == key){
+//                return arr[i].value;
+//            }
+//            //modify
+//        }
+//        return "";
+//    }
+//    boolean contains(int key){
+////        for(int i = 0; i < size ; i++){
+//        if(arr[probe(key)].key == key)
+//            return true;
+////        }
+//        return false;
+//    }
     public static void main(String[] args) {
 
         DictionaryUsingHasingLinearProbing aDictionary = new DictionaryUsingHasingLinearProbing(); // built-in Java dictionary
--- Pair.java
+++ Pair.java
@@ -2,6 +2,11 @@ public class Pair {
     int key;
     String value;
 
+
+    public Pair(){
+        value = "";
+        key = -1;
+    }
     public Pair(int key, String value) {
         this.key = key;
         this.value = value;
